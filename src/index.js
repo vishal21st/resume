9508fc622e595906979e24f16c98ccc4da5a2f35
vishal singhal <vishal@weinvest.net>
2019-02-05 21:06:21 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import "./css/style.scss"
+import { Companies, Projects, PersonalDetails, Skills, Languages } from './js/store'
+const CompaniesTpl = require("./js/templates/CompanyList.hbs")
+const ProjectsTpl = require("./js/templates/Projects.hbs")
+const QualificationTpl = require("./js/templates/Qualification.hbs")
+const AsideSectionTpl = require("./js/templates/AsideSection.hbs")
+const PersonalDetailTpl = require("./js/templates/PersonalDetail.hbs")
+const PageHeadingTpl = require("./js/templates/PageHeading.hbs")
+document.getElementById('company-section').innerHTML = CompaniesTpl({items: Companies});
+// document.getElementById('project-section').innerHTML = ProjectsTpl({items: Projects});
+document.getElementById('qualification-section').innerHTML = QualificationTpl();
+document.getElementById('skill-section').innerHTML = AsideSectionTpl(Skills);
+document.getElementById('language-section').innerHTML = AsideSectionTpl(Languages);
+document.getElementById('personal-detail-section').innerHTML = PersonalDetailTpl(PersonalDetails);
+document.getElementById('page-heading').innerHTML = PageHeadingTpl(PersonalDetails)
---
+++ src/js/store.js
@@ -0,0 +1,129 @@
+export const Companies = [
+  {
+    name: "Weinvest",
+    tenure: "Aug 2016 - Present",
+    link: "https://weinvest.com",
+    role: "Senior Software Engineer",
+    logo_link: "images/weinvest.png",
+    details: "A fintech startup based out of Singapore. I work here as a full stack Engineer, Also leading the UI team. We use Ruby on Rails for Back-End and  React, Vue, SASS for Front-End. All are services are deployed on AWS."
+  },
+  {
+    name: "Pipecast",
+    tenure: "Oct 2015 - Aug 2016",
+    link: "http://pipecast.in",
+    role: "Co-founder",
+    logo_link: "images/pipecast.png",
+    details: "A video streaming and collaboration platform for Video creators. We have 2 platforms Pipecast Stream [http://pipecast.in] and Pipecast Colab [http://colab.pipecast.in]. I developed the Front End for both platforms in Angualr js."
+  },
+  {
+    name: "Eltropy Inc",
+    tenure: "May 2015 - Oct 2015",
+    link: "https://www.eltropy.com/",
+    role: "Web Developer",
+    logo_link: "images/eltropy.png",
+    details: "A Silicon Valley startup with its development center in Bangalore. Provide smart solutions to B2B collateral. I mostly worked on feature building and enhancements. Also moved the knockout.js code to Angular js."
+  },
+  {
+    name: "Bang The Table",
+    tenure: "Aug 2014 - Apr 2015",
+    link: "https://www.bangthetable.com/",
+    role: "Web Developer",
+    logo_link: "images/btt.png",
+    details: "Bang the table is an Australia based startup providing community engagement solutions to government and local companies.My role was to work closely with product team and implement new features and constantly improve existing ones."
+  },
+  {
+    name: "Tech Mahindra",
+    tenure: "Jun 2012 - Aug 2014",
+    link: "https://www.techmahindra.com",
+    role: "Technical Associate",
+    logo_link: "images/tech_m.gif",
+    details: "Started my career with Tech Mahindra as a Software developer. Worked on  a Release Automation Tool (NOLIO). Provided support to Tesco's Devops cell to look after the release automation and trained people on Nolio tool."
+  }
+]
+
+export const Projects = [
+  {
+    name: "Some name",
+    role: "Some role",
+    details: "some desc"
+  }
+]
+
+export const PersonalDetails = {
+  first_name: "Vishal",
+  last_name: "Singhal",
+  role: "Web Developer",
+  email: "vishal.singhal21st@gmail.com",
+  nationality: "Indian",
+  dob: "7 july 1990",
+  contact: "+91-7829354321",
+  location: "Bangalore, India"
+}
+
+export const Skills = {
+  heading: "SKILLS",
+  items: [
+    {
+      name: "React / Redux",
+      level: "Intermediate"
+    },
+    {
+      name: "Vuejs / Vuex",
+      level: "Advance"
+    },
+    {
+      name: "Angular",
+      level: "Advance"
+    },
+    {
+      name: "React Native",
+      level: "Beginner"
+    },
+    {
+      name: "Ruby on Rails",
+      level: "Intermediate"
+    },
+    {
+      name: "JAVASCRIPT",
+      level: "Advance"
+    },
+    {
+      name: "HTML / CSS",
+      level: "Advance"
+    },
+    {
+      name: "SASS",
+      level: "Advance"
+    },
+    {
+      name: "Github",
+      level: "Advance"
+    },
+    {
+      name: "AWS",
+      level: "Intermediate"
+    },
+    {
+      name: "Webpack",
+      level: "Advance"
+    },
+    {
+      name: "Chrome Extension",
+      level: "Advance"
+    }
+  ]
+}
+
+export const Languages = {
+  heading: "LANGUAGES",
+  items: [
+    {
+      name: "English",
+      level: "Fluent"
+    },
+    {
+      name: "Hindi",
+      level: "Fluent"
+    }
+  ]
+}
